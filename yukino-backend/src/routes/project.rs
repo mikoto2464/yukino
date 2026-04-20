7494a8b41ec939085421bb446aa8892e51a575e7
mikoto <misaka.mikoto2464@gmail.com>
2026-04-20 22:24:51 +0800

    :bug: 修改/api/project/all的请求方法为get

--- yukino-backend/src/routes/project.rs
+++ yukino-backend/src/routes/project.rs
@@ -2,13 +2,13 @@ use crate::auth::Backend;
 use crate::handlers;
 use crate::state::YukinoState;
 use axum::Router;
-use axum::routing::{delete, post};
+use axum::routing::{get, post};
 use axum_login::login_required;
 use std::sync::Arc;
 
 pub fn project_routes() -> Router<Arc<YukinoState>> {
     let need_login_router = Router::new()
-        .route("/all", delete(handlers::project::get_projects))
+        .route("/all", get(handlers::project::get_projects))
         .route_layer(login_required!(Backend, login_url = "/login"));
 
     Router::new()
