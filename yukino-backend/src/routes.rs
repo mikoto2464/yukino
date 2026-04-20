17a2e07787d0c986d1b182d9a75b5ff2212d407d
mikoto <misaka.mikoto2464@gmail.com>
2026-04-20 22:24:24 +0800

    :memo: 编写接口文档

--- yukino-backend/src/routes.rs
+++ yukino-backend/src/routes.rs
@@ -16,6 +16,38 @@ async fn handler_404() -> impl IntoResponse {
     YukinoError::NotFound("This path does not exist.".to_string())
 }
 
+/// 接口一览:
+/// POST /api/auth/telegram/callback 接收telegram返回的auth信息并登录
+/// Payload
+/// id: i64,
+/// first_name: String
+/// last_name: String
+/// username: String
+/// photo_url: String
+/// hash: String
+/// auth_date: i64
+///
+/// TODO POST /api/auth/qq/callback 接收qq返回的auth信息并登录
+///
+/// DELETE /api/user/session (LOGIN_REQUIRED)关闭会话，退出登录
+///
+/// GET /api/user/me (LOGIN_REQUIRED)获取用户基本信息
+///
+/// GET /api/device (LOGIN_REQUIRED)获取用户已绑定的所有设备
+///
+/// POST /api/device (LOGIN_REQUIRED)创建设备
+/// Payload
+/// hardware_id: String,
+/// name: String,
+///
+/// DELETE /api/device/{hardware_id} (LOGIN_REQUIRED)删除用户绑定的设备
+///
+/// POST /api/project 创建项目，前端无需调用
+/// Payload
+/// create_project_key: String,
+/// name: String,
+///
+/// GET /api/project/all (LOGIN_REQUIRED)获取用户已订阅的所有项目
 pub fn app_routers(
     state: Arc<YukinoState>,
     auth_layer: AuthManagerLayer<Backend, SqliteStore>,
