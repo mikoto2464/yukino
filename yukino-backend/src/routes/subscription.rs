81ce4ad25d896efbd8c4523ecdddf54c4916133f
mikoto <misaka.mikoto2464@gmail.com>
2026-04-30 13:42:17 +0800

    :bug: 修复subscription_routes未添加login_required layer的问题

--- yukino-backend/src/routes/subscription.rs
+++ yukino-backend/src/routes/subscription.rs
@@ -3,7 +3,11 @@ use crate::state::YukinoState;
 use axum::Router;
 use axum::routing::post;
 use std::sync::Arc;
+use axum_login::login_required;
+use crate::auth::Backend;
 
 pub fn subscription_routes() -> Router<Arc<YukinoState>> {
-    Router::new().route("/", post(handlers::subscription::redemption))
+    Router::new()
+        .route("/", post(handlers::subscription::redemption))
+        .route_layer(login_required!(Backend, login_url = "/login"))
 }
